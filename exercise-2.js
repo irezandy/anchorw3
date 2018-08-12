71b7c41c798b3f1c3c7dc4a73d12b4f80a3a93d7
ihsan rezandy <irezandy@gmail.com>
2018-08-12 20:16:09 +0700

    upload exercise 2 tantangan array 1

---
+++ exercise-2.js
@@ -0,0 +1,9 @@
+function balikkata(str){
+  var terbalik = "";    
+  for (var i = str.length - 1; i >= 0; i--){        
+    terbalik += str[i];
+  }    
+  return terbalik;
+}
+
+console.log(balikkata('Hello World!'))
